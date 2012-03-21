557cbea9e19bec9391dac3ecec20abaa9f1a23f1
Ian Clarkson <clarksonian@gmail.com>
2012-03-21 00:09:09 -0400

    Adding from an array in listings.js. As long as we match the format, we should be able to show everything in our database!

--- display/listings.js
+++ display/listings.js
@@ -1,2 +1,3 @@
-var prop1 = new google.maps.LatLng(43.468774, -80.539312);
-var prop2 = new google.maps.LatLng(43.468774, -80.539312);
+var listings = new Array();
+listings[0] = { pos: new google.maps.LatLng(43.468774, -80.539312), name: "Test1" };
+listings[1] = { pos: new google.maps.LatLng(43.468774, -80.538), name: "Test2" };
