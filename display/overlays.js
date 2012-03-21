4140bee92bb27bb3b829c635089679ed1043c1e3
Ian Clarkson <clarksonian@gmail.com>
2012-03-21 16:28:49 -0400

    The bounded rectangles appear to be working. Nice! We can see if they contain points and everything, which is just what we wanted!

--- display/overlays.js
+++ display/overlays.js
@@ -4,42 +4,58 @@ function drawOverlays(map, centre, listings) {
 	// total coverage is (2*blocks+1)*res both NS and EW
 	// we want the number to be odd so that we can centre it
 	// on the centre given to us
-	// Catchment is the radius wherein a circle will include
-	// properties within it for price averaging
 	var res = 400;
 	var blocks = 4;
-	var catchment = Math.sqrt(res * res + res * res) / 2;
 
 	// Create the centres by adjusting from the centre
 	// using a heading
+	// The bounds are the rectangle bounds
 	var centres = new Array();
+	var bounds  = new Array();
 	var i, j;
 	for( i = -blocks; i <= blocks; i++) {
 		
 		centres[i + blocks] = new Array();
+		bounds [i + blocks] = new Array();
 		
 		for( j = -blocks; j <= blocks; j++) {
 			
-			var offlat = google.maps.geometry.spherical.computeOffset(centre, res * i, 180);
-			var offlng = google.maps.geometry.spherical.computeOffset(centre, res * j, 90);
+			var offlat;
+			var offlng;
 			
+			offlat = google.maps.geometry.spherical.computeOffset(centre, res * i, 180);
+			offlng = google.maps.geometry.spherical.computeOffset(centre, res * j, 90);
 			centres[i + blocks][j + blocks] = new google.maps.LatLng(offlat.lat(), offlng.lng());
+			
+			// calculate the corner coordinates
+			// these offsets are shifted a bit
 
+			// southwest
+			offlat = google.maps.geometry.spherical.computeOffset(centre, res * (i + 0.5), 180);
+			offlng = google.maps.geometry.spherical.computeOffset(centre, res * (j - 0.5), 90);
+			var sw = new google.maps.LatLng(offlat.lat(), offlng.lng());
+			
+			// northeast
+			offlat = google.maps.geometry.spherical.computeOffset(centre, res * (i - 0.5), 180);
+			offlng = google.maps.geometry.spherical.computeOffset(centre, res * (j + 0.5), 90);
+			var ne = new google.maps.LatLng(offlat.lat(), offlng.lng());
+			
+			// set the bounds
+			bounds[i + blocks][j + blocks] = new google.maps.LatLngBounds(sw, ne);
+			
 			// Temporarily show markers as debug tool
 			// new google.maps.Marker({
 				// position : centres[i + blocks][j + blocks],
 				// map : map,
 				// title : "" + j
 			// });
-			
-			// Draw the catchment circles
-			new google.maps.Circle({
-				strokeWeight: 0,
-				map: map,
-				center : centres[i + blocks][j + blocks],
-				radius : catchment
-			});
 
+			// Draw rectangles
+			new google.maps.Rectangle({
+				strokeWeight: 0.1,
+				map: map,
+				bounds: bounds[i + blocks][j + blocks]
+			})
 
 		}
 	}
