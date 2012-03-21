32e349543078129be2e4b9d631a73778d09d1528
Ian Clarkson <clarksonian@gmail.com>
2012-03-21 17:51:45 -0400

    Calculating the overall max and min, and setting the color after the fact.

--- display/overlays.js
+++ display/overlays.js
@@ -24,7 +24,7 @@ function drawOverlays(map, centre, listings) {
 	// total coverage is (2*blocks+1)*res both NS and EW
 	// we want the number to be odd so that we can centre it
 	// on the centre given to us
-	var res = 400;
+	var res    = 400;
 	var blocks = 4;
 
 	// Centres are the rectangle centres
@@ -33,15 +33,22 @@ function drawOverlays(map, centre, listings) {
 	var centres = new Array();
 	var bounds  = new Array();
 	var windows = new Array();
+	var rects   = new Array();
 	
-	var i, j;
-	for( i = -blocks; i <= blocks; i++) {
+	// Need these to determine what colours the rectangles should be
+	// That also means we can't do the rectangles until we've gone through
+	// the loop once calculating values
+	var whole_min = null;
+	var whole_max = null;
+	
+	for (var i = -blocks; i <= blocks; i++) {
 		
 		centres[i + blocks] = new Array();
 		bounds [i + blocks] = new Array();
 		windows[i + blocks] = new Array();
+		rects  [i + blocks] = new Array();
 		
-		for( j = -blocks; j <= blocks; j++) {
+		for (var j = -blocks; j <= blocks; j++) {
 			
 			var offlat;
 			var offlng;
@@ -70,8 +77,9 @@ function drawOverlays(map, centre, listings) {
 			// while we're at it, find the min and max
 			var sum = 0;
 			var num = 0;
-			var min = 0;
-			var max = 0;
+			var min = null;
+			var max = null;
+			
 			for (var k = 0; k < listings.length; k++) {
 				
 				if (bounds[i+blocks][j+blocks].contains(listings[k].pos)) {
@@ -80,32 +88,53 @@ function drawOverlays(map, centre, listings) {
 					if (listings[k].price == null) continue;
 						
 					// find min and max
-					if (min == 0 || listings[k].price < min) {
+					if (min == null || listings[k].price < min) {
 						min = listings[k].price;
 					}
-					if (max == 0 || listings[k].price > max) {
+					if (max == null || listings[k].price > max) {
 						max = listings[k].price;
 					}						 
 					
 					sum += listings[k].price;
 					num++;
 				}
-			} 
+			}
 			
-			// Draw rectangles based on the rent colour
-			// A colour of null means we shouldn't draw it at all
-			var rect = new google.maps.Rectangle({
+			// Draw the rect based on the number of properties within it
+			// We will adjust its fill colour after we know the whole min and max
+			rects[i + blocks][j + blocks] = new google.maps.Rectangle({
 				strokeWeight : 0.1,
-				fillColor: "#FF0000",
-				fillOpacity: num > 0 ? 0.25 : 0.0,
+				fillColor : "#FF0000",
+				fillOpacity : num > 0 ? 0.25 : 0.0,
 				map : map,
 				bounds : bounds[i + blocks][j + blocks]
 			})
 
-			if (num > 0) {
-				// Attach a window to each rectangle
-				attachWindow(map, rect, centres[i+blocks][j + blocks], sum / num, min, max);
+			// Attach a window to each rectangle if there are properties in it
+			if(num > 0) {
+				attachWindow(map, rects[i + blocks][j + blocks], centres[i+blocks][j + blocks], sum / num, min, max);
 			}
+
+			// Adjust the whole max/min
+			if(whole_min == null || (min != null && min < whole_min)) {
+				whole_min = min;
+			}
+			if(whole_max == null || (max != null && max > whole_max)) {
+				whole_max = max;
+			}
+	 
 		}
 	}
+	
+	// Draw rectangles based on the rent colour
+	// A colour of null means we shouldn't draw it at all
+	for (var i = -blocks; i <= blocks; i++) {
+		for (var j = -blocks; j <= blocks; j++) {
+			rects[i + blocks][j + blocks].setOptions({
+				fillColor : "#00FF00"
+			})
+		}
+	}
+			
+
 }
