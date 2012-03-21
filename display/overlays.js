81ba55a2aa19429a4f063105f840bbf51a979372
Ian Clarkson <clarksonian@gmail.com>
2012-03-21 16:17:06 -0400

    Adding the geometry library to calculate distances. Drawing catchment circles. Going to try switching it to rectangles.

--- display/overlays.js
+++ display/overlays.js
@@ -1,17 +1,46 @@
 function drawOverlays(map, centre, listings) {
-	var triangleCoords = [new google.maps.LatLng(25.774252, -80.190262), new google.maps.LatLng(18.466465, -66.118292), new google.maps.LatLng(32.321384, -64.75737), new google.maps.LatLng(25.774252, -80.190262)];
 
-	// Construct the polygon
-	// Note that we don't specify an array or arrays, but instead just
-	// a simple array of LatLngs in the paths property
-	bermudaTriangle = new google.maps.Polygon({
-		paths : triangleCoords,
-		strokeColor : "#FF0000",
-		strokeOpacity : 0.8,
-		strokeWeight : 2,
-		fillColor : "#FF0000",
-		fillOpacity : 0.35
-	});
+	// set up values that dictate the resolution of the grid
+	// total coverage is (2*blocks+1)*res both NS and EW
+	// we want the number to be odd so that we can centre it
+	// on the centre given to us
+	// Catchment is the radius wherein a circle will include
+	// properties within it for price averaging
+	var res = 400;
+	var blocks = 4;
+	var catchment = Math.sqrt(res * res + res * res) / 2;
 
-	bermudaTriangle.setMap(map);
+	// Create the centres by adjusting from the centre
+	// using a heading
+	var centres = new Array();
+	var i, j;
+	for( i = -blocks; i <= blocks; i++) {
+		
+		centres[i + blocks] = new Array();
+		
+		for( j = -blocks; j <= blocks; j++) {
+			
+			var offlat = google.maps.geometry.spherical.computeOffset(centre, res * i, 180);
+			var offlng = google.maps.geometry.spherical.computeOffset(centre, res * j, 90);
+			
+			centres[i + blocks][j + blocks] = new google.maps.LatLng(offlat.lat(), offlng.lng());
+
+			// Temporarily show markers as debug tool
+			// new google.maps.Marker({
+				// position : centres[i + blocks][j + blocks],
+				// map : map,
+				// title : "" + j
+			// });
+			
+			// Draw the catchment circles
+			new google.maps.Circle({
+				strokeWeight: 0,
+				map: map,
+				center : centres[i + blocks][j + blocks],
+				radius : catchment
+			});
+
+
+		}
+	}
 }
