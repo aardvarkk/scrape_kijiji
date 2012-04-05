fb45c66fdc75793e9d2791a4967e8af3dbbfcfc0
unknown <clarkson@GESTURETESTER.na.qualcomm.com>
2012-04-05 16:54:21 -0400

    Changing order to alphabetical.

--- scraper/locations.py
+++ scraper/locations.py
@@ -8,9 +8,9 @@ Created on Apr 5, 2012
 listings_prefix = "listings_"
 
 # can have multiple locations
-rss = { "hamilton" : "http://hamilton.kijiji.ca/f-SearchAdRss?AdType=2&CatId=36&Location=80014",
+rss = { "guelph"   : "http://guelph.kijiji.ca/f-SearchAdRss?AdType=2&CatId=36&Location=1700242",
+        "hamilton" : "http://hamilton.kijiji.ca/f-SearchAdRss?AdType=2&CatId=36&Location=80014",
         "london"   : "http://london.kijiji.ca/f-SearchAdRss?AdType=2&CatId=36&Location=1700214", 
-        "guelph"   : "http://guelph.kijiji.ca/f-SearchAdRss?AdType=2&CatId=36&Location=1700242",
         "waterloo" : "http://kitchener.kijiji.ca/f-SearchAdRss?AdType=2&CatId=36&Location=1700209"
 } 
 
