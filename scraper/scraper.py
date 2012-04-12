50d12ff2b693e6e46f37e0df2c28259391d21190
Ian Clarkson <clarksonian@gmail.com>
2012-04-12 10:22:23 -0400

    Fixed an issue with links not being inserted properly into the database.

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -77,25 +77,30 @@ def add_new_listings(rss, c, geocode):
 
     # iterate through all the items
     for item in soup.findAll('item'):
-        c.execute('''SELECT COUNT(*) FROM listings WHERE guid = ?''', [item.guid.string])
+
+	guid = item.guid.string
+	link = item.link.nextSibling
+        pubdate = item.pubdate.string
+
+        c.execute('''SELECT COUNT(*) FROM listings WHERE guid = ?''', 
+[guid])
 
         # new listing
         # get more details from the link
         # then add it to the database
         if c.fetchone()[0] <= 0:
-            details = get_listing_details(item.link.nextSibling, geocode)
-            c.execute('''INSERT INTO listings (guid, link, pubdate, address, price, lat, lng) VALUES(?, ?, ?, ?, ?, ?, ?)''', [item.guid.string, item.link.string, item.pubdate.string, details['address'], details['price'], details['lat'], details['lng']])
+            details = get_listing_details(link, geocode)
+            c.execute('''INSERT INTO listings (guid, link, pubdate, 
+address, price, lat, lng) VALUES(?, ?, ?, ?, ?, ?, ?)''', 
+[guid, link, pubdate, 
+details['address'], details['price'], details['lat'], details['lng']])
             
             # print "Added listing", details['address'], details['price'], details['lat'], details['lng']
         
         # existing listing
         else:
             pass
-            
-        # print item.guid.string
-        # print item.link.string
-        # print item.pubdate.string
-        
+                    
 #        break
 
 def update(location, rss, db_file):
